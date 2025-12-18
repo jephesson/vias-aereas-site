3347a4a13950a72bda8649e19c218ad654128e40
Jephesson Alex Floriano dos Santos <jephessonalex@192.168.68.103>
2025-12-18 10:36:34 -0300

    Atualiza página Sobre com time, idealizador e Instagram

--- src/app/sobre/page.tsx
+++ src/app/sobre/page.tsx
@@ -1,4 +1,5 @@
 const CNPJ = "63.817.773/0001-85";
+const INSTAGRAM = "https://www.instagram.com/vias.aereas";
 
 export default function SobrePage() {
   return (
@@ -6,12 +7,44 @@ export default function SobrePage() {
       <div className="va-shell">
         <div className="va-card">
           <h1 className="va-title">Sobre o Vias Aéreas</h1>
+
           <p className="va-subtitle">
-            Somos uma agência focada em encontrar a melhor opção de passagem para sua viagem.
-            <br />
-            <b>Cotação em até 2 horas.</b>
+            O <b>Vias Aéreas</b> é uma agência especializada em cotações inteligentes
+            de passagens aéreas, criada com o propósito de ajudar pessoas a viajarem
+            pagando os <b>melhores preços possíveis</b>, seja utilizando milhas ou
+            tarifas tradicionais.
           </p>
-          <p className="va-subtitle">CNPJ: <b>{CNPJ}</b></p>
+
+          <p className="va-subtitle">
+            Somos um <b>grupo formado por quatro pessoas</b>, todos apaixonados por
+            viagens e por encontrar oportunidades reais de economia:
+            <br />
+            • Eduarda Santos <br />
+            • Lucas Araújo <br />
+            • Paola Ziani <br />
+            • Gabriel Silva
+          </p>
+
+          <p className="va-subtitle">
+            A empresa foi idealizada pelo <b>Dr. Jephesson Santos</b>, farmacêutico,
+            doutor e empreendedor, responsável pelo desenvolvimento do site,
+            estrutura tecnológica e pela visão estratégica do projeto.
+          </p>
+
+          <p className="va-subtitle">
+            Nosso foco é oferecer um atendimento <b>rápido, transparente e humano</b>,
+            com cotações geralmente respondidas em até <b>2 horas</b>, sempre buscando
+            a melhor alternativa para cada perfil de viajante.
+          </p>
+
+          <p className="va-subtitle">
+            <b>CNPJ:</b> {CNPJ}
+            <br />
+            <b>Instagram:</b>{" "}
+            <a href={INSTAGRAM} target="_blank" rel="noopener noreferrer">
+              @vias.aereas
+            </a>
+          </p>
         </div>
       </div>
     </main>
